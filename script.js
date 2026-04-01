1e3c15a9f6f338f63adcbf55b5d31d1a8d3e39d4
erickctscrr-web <erickcts.crr@gmail.com>
2026-03-31 19:46:47 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Hola, Erick. JavaScript está funcionando.");
+function agendarCita(){
+    alert("Gracias por agendar tu cita, Te contactaremos por WhatsApp.")
+}
+
+let nombre = "Erick"
+let edad = 18;
+
+if(edad >=18){
+    console.log(nombre + "puede entrar a la barbería solo.");
+}else{
+    console.log(nomre + "necesita permiso.");
+}
